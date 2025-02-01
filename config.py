58cb243145cefb297cb0830149007a7e6cd5e7c9
YourSohit <111377504+YourSohit@users.noreply.github.com>
2025-02-01 16:29:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,177 @@
 # API credentials
-API_ID = 123456
-API_HASH = "DWSC65HGFswcf56g8ergjhgdw54586"
-BOT_TOKEN = "123456:abcdekgfiJLKMNOPqjngghdf"
+API_ID = 17760082
+API_HASH = "c3fc3cd44886967cf3c0e8585b5cad1c"
+BOT_TOKEN = "5066445400:AAEGWQO2Ov6SNqaW5mcPwnkr6bUFatdiKtY"
 
 # Redis configuration
 REDIS_CONFIG = {
-    "HOST": "redis-12345.c16.us-east-1-2.ec2.cloud.redislabs.com",
-    "PORT": 17713,
+    "HOST": "redis-19880.c232.us-east-1-2.ec2.redns.redis-cloud.com",
+    "PORT": 19880,
     "USERNAME": "default",
-    "PASSWORD": "password",
-    "DATABASE": "NTM"
+    "PASSWORD": "9UpI7sfCcaAyLoOPdVUhHk0gZm5pAJ07",
+    "DATABASE": "TCP"
 }
 
 # Chat ID where videos will be stored
-PRIVATE_CHAT_ID = -100123456789
+PRIVATE_CHAT_ID = -1002423281302
 
 # Authentication cookie
 COOKIE = {
-    "COOKIE": "Paste your cookie here"
+    "COOKIE": "[
+{
+    "domain": ".1024terabox.com",
+    "expirationDate": 1772906787.895271,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "__bid_n",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "194bd88541ee755edd4207",
+    "id": 1
+},
+{
+    "domain": ".1024terabox.com",
+    "expirationDate": 1772906788.017661,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "_ga",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "GA1.1.572991284.1738346633",
+    "id": 2
+},
+{
+    "domain": ".1024terabox.com",
+    "expirationDate": 1772906825.192429,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "_ga_06ZNKL8C2E",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "GS1.1.1738346632.1.1.1738346825.7.0.0",
+    "id": 3
+},
+{
+    "domain": ".1024terabox.com",
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "ab_sr",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": true,
+    "storeId": "0",
+    "value": "1.0.1_OTVlODcwMzY1ZmM4ODI5OGJmNzkxYzAxYWM4OTk4MjkxNGIwOTQ2OGYwZjkwOTA4ZTY3YzdmMzdhZmEzYTk3NTk5Y2ZjNDdlMjc2NWM0NGUzMWVjZWY0Njc4NDU2ZmRkM2E4NzBmOTNlOWZkNzVlNTU4ZWI0NThmM2ViY2QxMjVhMmRkZmQ0MDRiOGY1ZmNmNjNhOWFkMjRkMmNmZWZiMQ==",
+    "id": 4
+},
+{
+    "domain": ".1024terabox.com",
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "ab_ymg_result",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": true,
+    "storeId": "0",
+    "value": "{\"data\":\"3b2a590e6743461cec71198b6d9c34a1cf91c2cb91b6e3831221f1f532cebc3357351aa82f89c1314d50d63a3346876b72e6264e6de0c50025eb9b9807df48ee17c6b119438752841ae5e4f9fc94d0b5f9bd5304750d238590b1468031a851ef615b225a487159b2807d66b51a0346e33ae9c57e92882a21c74fd381b56d62b3\",\"key_id\":\"66\",\"sign\":\"4d245811\"}",
+    "id": 5
+},
+{
+    "domain": ".1024terabox.com",
+    "expirationDate": 1743530630.33906,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "browserid",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "WvMPhQQagSFKmLcA9f5gRTch42Sw4kGqRdJykeGDeDAWwYQmUjFMSNEWlkaJp3hFJOjGP2zYrk9n0QXy",
+    "id": 6
+},
+{
+    "domain": ".1024terabox.com",
+    "expirationDate": 1740938679.595576,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "lang",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "en",
+    "id": 7
+},
+{
+    "domain": ".1024terabox.com",
+    "expirationDate": 1769882644.72342,
+    "hostOnly": false,
+    "httpOnly": true,
+    "name": "ndus",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": true,
+    "session": false,
+    "storeId": "0",
+    "value": "Y-2t3gxteHuiyVkVb_EDyeYGRMcTNcxXegQqiAGC",
+    "id": 8
+},
+{
+    "domain": "www.1024terabox.com",
+    "hostOnly": true,
+    "httpOnly": false,
+    "name": "csrfToken",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": true,
+    "storeId": "0",
+    "value": "dtJ6S9Yofu6TDkt_W7grP5sY",
+    "id": 9
+},
+{
+    "domain": "www.1024terabox.com",
+    "expirationDate": 1753898644,
+    "hostOnly": true,
+    "httpOnly": false,
+    "name": "g_state",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "{\"i_l\":0}",
+    "id": 10
+},
+{
+    "domain": "www.1024terabox.com",
+    "expirationDate": 1740938787,
+    "hostOnly": true,
+    "httpOnly": false,
+    "name": "ndut_fmt",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "679FDBCB23A7B543128CACD16EA09A3073685C40A85EDD7D7CF0195D14E450A4",
+    "id": 11
+}
+]"
 }
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
 # List of bot admins
-ADMINS = [803003146]
+ADMINS = [1809710185]
